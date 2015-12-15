a3fda2c0563839192384c98891c177eba0234ebd
Alberto Revel <alberto.revel@gmail.com>
2015-12-15 13:43:37 +0100

    Deleted other repos

--- demo/src/main/java/urlshortener2015/demo/web/UrlShortenerControllerWithLogs.java
+++ demo/src/main/java/urlshortener2015/demo/web/UrlShortenerControllerWithLogs.java
@@ -4,14 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import urlshortener2015.common.domain.ShortURL;
 import urlshortener2015.common.web.UrlShortenerController;
