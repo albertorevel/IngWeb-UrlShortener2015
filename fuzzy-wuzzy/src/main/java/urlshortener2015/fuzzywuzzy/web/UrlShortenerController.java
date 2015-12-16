5b5b9ae57170124f8d81faad36447cd2b8fa144f
Alberto Revel <alberto.revel@gmail.com>
2015-12-16 02:06:16 +0100

    Added our qrGenerator

--- fuzzy-wuzzy/src/main/java/urlshortener2015/fuzzywuzzy/web/UrlShortenerController.java
+++ fuzzy-wuzzy/src/main/java/urlshortener2015/fuzzywuzzy/web/UrlShortenerController.java
@@ -1,8 +1,14 @@
 package urlshortener2015.fuzzywuzzy.web;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 
 import com.google.common.hash.Hashing;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Writer;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +28,10 @@ import urlshortener2015.fuzzywuzzy.repository.ShortURLRepository;
 import urlshortener2015.fuzzywuzzy.domain.Click;
 import urlshortener2015.fuzzywuzzy.domain.ShortURL;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -33,10 +43,15 @@ import static org.apache.tomcat.util.codec.binary.Base64.encodeBase64String;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+
 @RestController
 public class UrlShortenerController {
 	private static final Logger log = LoggerFactory
 			.getLogger(urlshortener2015.fuzzywuzzy.web.UrlShortenerController.class);
+
+	private String content;
+	private byte[] byteArray;
+
 	@Autowired
 	protected ShortURLRepository shortURLRepository;
 
@@ -101,7 +116,8 @@ public class UrlShortenerController {
 							id, null)).toUri();
 			String qrApi = createQrQuery(uri, vCardName, correction);
 			RestTemplate restTemplate = new RestTemplate();
-			String qrDef = encodeBase64String(restTemplate.getForObject(qrApi, byte[].class));
+//			String qrDef = encodeBase64String(restTemplate.getForObject(qrApi, byte[].class));
+			String qrDef = encodeBase64String(byteArray);
 			ShortURL su = new ShortURL(id, url,
 					uri, sponsor, new Date(
 					System.currentTimeMillis()), owner,
@@ -111,25 +127,64 @@ public class UrlShortenerController {
 			return null;
 		}
 	}
+
+
 	private String createQrQuery(URI uri, String vCardName, String correction) {
-		String query = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&choe=UTF-8&chl=";
+		String query = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&choe=UTF-8";
 		if (vCardName == null) {
-			query += uri;
+			content = uri.toString();
 		} else {
 			try {
 				vCardName = URLEncoder.encode(vCardName, "UTF-8");
-				query += "BEGIN%3AVCARD%0AVERSION%3A4.0%0AN%3A" + vCardName +
+				content = "BEGIN%3AVCARD%0AVERSION%3A4.0%0AN%3A" + vCardName +
 						"%0AURL%3A" + uri + "%0AEND%3AVCARD";
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
-				query = null;
+				return null;
 			}
 
 		}
 		if (correction != null) {
 			query += "&chld=" + correction;
 		}
-		return query;
+		BitMatrix matrix = null;
+		Writer writer = new QRCodeWriter();
+
+		try {
+			matrix = writer.encode(content, BarcodeFormat.QR_CODE, 400, 400);
+		} catch (WriterException e) {
+			e.printStackTrace();
+		}
+
+		BufferedImage image = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
+		// Iterate through the matrix and draw the pixels to the image
+		int hexa1 = 0xff0000;
+		int hexa2 = 0xFFFFFF;
+		for (int y = 0; y < 400; y++) {
+			for (int x = 0; x < 400; x++) {
+				int grayValue = (matrix.get(x, y) ? 0 : 1) & 0xff;
+				image.setRGB(x, y, (grayValue == 0 ? hexa1 : hexa2));
+			}
+		}
+
+
+//		try {
+//			FileOutputStream qrCode = new FileOutputStream("W:/qrcode.png");
+//			ImageIO.write(image, "png", qrCode);
+//			qrCode.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			ImageIO.write(image, "png", baos);
+			byteArray = baos.toByteArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return query + "&chl=" +content;
 
 	}
 	protected String extractIP(HttpServletRequest request) {
