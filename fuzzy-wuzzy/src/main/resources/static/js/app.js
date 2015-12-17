6ab57d0efefb9975e3aa7f390f89c6248f0ec2eb
christianjaka94 <christiangar_94@hotmail.com>
2015-12-17 16:54:42 +0100

    travis passed AGAIN?

--- fuzzy-wuzzy/src/main/resources/static/js/app.js
+++ fuzzy-wuzzy/src/main/resources/static/js/app.js
@@ -24,26 +24,6 @@ $(document).ready(
                         $("#result").html(
                                 "<div class='alert alert-danger lead'>ERROR</div>");
                     }
-                    type : "GET",
-                    url : "/publicidad",
-                    data : $(this).serialize(),
-                    success : function(msg) {
-                        $("#result").html(
-                            "<div class='alert alert-success lead'><a target='_blank' href='"
-                            + msg.uri
-                            + "'>"
-                            + msg.uri
-                            + "</a></div><br/></div>"
-                            +"<div tiles:qr>"
-                            +"<img src='data:image/png;base64, "
-                            + msg.qrCode
-                            + "' alt = 'Red Dot'/> "
-                            + msg.qrApi+"</div>");
-                    },
-                    error : function() {
-                        $("#result").html(
-                                "<div class='alert alert-danger lead'>ERROR</div>");
-                    }
                 });
             });
     });
